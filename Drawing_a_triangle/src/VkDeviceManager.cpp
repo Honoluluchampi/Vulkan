ded0686b21e91ba4ad7e28161441bd832b081c7f
Honoluluchampi <msd13aj@gmail.com>
2022-01-25 23:48:26 +0900

    complete physical device

--- Drawing_a_triangle/src/VkDeviceManager.cpp
+++ Drawing_a_triangle/src/VkDeviceManager.cpp
@@ -26,12 +26,48 @@ void VkDeviceManager::pickPhysicalDevice(VkInstance& instance)
     }
 }
 
-bool VkDeviceManager::isDeviceSuitable(const VkPhysicalDevice& device) {
+bool VkDeviceManager::isDeviceSuitable(const VkPhysicalDevice& device) 
+{
     // VkPhysicalDeviceProperties deviceProperties; 
     // VkPhysicalDeviceFeatures deviceFeatures; 
     // vkGetPhysicalDeviceProperties(device, &deviceProperties); 
     // vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
     // return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU 
     //     && deviceFeatures.geometryShader;
-    return true;
+    QueueFamilyIndices indices = findQueueFamilies(device);
+    return indices.isComplete();
+}
+
+VkDeviceManager::QueueFamilyIndices VkDeviceManager::findQueueFamilies
+    (const VkPhysicalDevice& device)
+{
+    VkDeviceManager::QueueFamilyIndices indices;
+    // retrieve the list of quque families 
+    uint32_t queueFamilyCount = 0;
+    // use same function
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+    // vkQueueFamilyProperties struct contains some details about the queue family
+    // the type of operations, the number of queue that can be created based on that family
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+    // check whether at least one queueFamily support VK_QUEUEGRAPHICS_BIT
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies){
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily_m = i;
+            break;
+        }
+        i++;
+    }
+    return indices;
+}
+
+inline bool VkDeviceManager::QueueFamilyIndices::isComplete()
+{
+    return graphicsFamily_m.has_value();
+}
+
+void VkDeviceManager::createLogicalDevice()
+{
+    
 }
--- Drawing_a_triangle/src/VkDeviceManager.hpp
+++ Drawing_a_triangle/src/VkDeviceManager.hpp
@@ -1,13 +1,25 @@
 #pragma once
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <optional>
 
 class VkDeviceManager
 {
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily_m;
+        inline bool isComplete();
+    };
 public:
+    // relevant to physicaldevice
     void pickPhysicalDevice(VkInstance& instance);
-    bool isDeviceSuitable(const VkPhysicalDevice& device);
+    QueueFamilyIndices findQueueFamilies(const VkPhysicalDevice& device);
+    // relevant to logical device
+    void createLogicalDevice();
+    
 private:
+    bool isDeviceSuitable(const VkPhysicalDevice& device);
     // implicitly destroyed when vkInstance is destroyed
     VkPhysicalDevice physicalDevice_m = VK_NULL_HANDLE;
+    VkDevice divice_m;
 };
