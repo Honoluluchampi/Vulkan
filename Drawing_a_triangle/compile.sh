f27dabbd40dc3e782aab321f71ff5018c3c3fade
Honoluluchampi <msd13aj@gmail.com>
2022-02-25 21:11:16 +0900

    ubuntu env (enabled khr layer)

--- Drawing_a_triangle/compile.sh
+++ Drawing_a_triangle/compile.sh
@@ -1,5 +1,11 @@
-COMPILER="/Users/toyotariku/VulkanSDK/1.2.198.1/macOS/bin/glslc"
+OS="Ubuntu" #"Mac"
+if [ $OS = "Mac" ]; then
+    COMPILER="/Users/toyotariku/VulkanSDK/1.2.198.1/macOS/bin/glslc"
+else
+    COMPILER="/home/honolulu/programs/downloaded-libraries/vulkan1.3.204.0/x86_64/bin/glslc"
+fi
 SHADERDIR="./src/shader"
+mkdir -p spv
 OUTPUTDIR="./spv"
 $COMPILER $SHADERDIR/shader.vert -o $OUTPUTDIR/vert.spv
 $COMPILER $SHADERDIR/shader.frag -o $OUTPUTDIR/frag.spv
